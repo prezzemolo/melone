63cc841871b93a876ea5abc57ffc126fc81e8245
prezzemolo <git@prezzemolo.ga>
2016-09-26 01:00:35 +0900

    fixer

--- ip.py
+++ ip.py
@@ -3,17 +3,17 @@
 import melone
 
 # initialize class
-pomodori = melone.pomodori('http://cherry.prezzemolo.ga/mos/')
+pomodori = melone.pomodori('http://cherry.prezzemolo.ga/')
 
 # retrive globalip
 try:
     ip = pomodori.globalIP()
 except melone.error.HTTPError as e:
     print("HTTPError: %d %s" % (e.code, e.reason))
-#    try:
-    print("APIError: %d %s" % (e.response['status'], e.response['description']))
-#    except:
-#        print("Responce: %s" % (e.response))
+    try:
+        print("APIError: %d %s" % (e.response['status'], e.response['description']))
+    except:
+        print("Responce: %s" % (e.response))
     exit(1)
 except melone.error.URLError as e:
     print("URLError: %s" % (e.reason))
